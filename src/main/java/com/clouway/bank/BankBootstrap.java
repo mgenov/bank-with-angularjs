064867f6784c94d6be3910292b7dd435b2cd7eec
Miroslav Genov <mgenov@gmail.com>
2017-01-04 13:50:24 +0200

    bank: initial java project skeleton
    Added a sample java app which uses Jetty to serve the static content in
    the static folder.

---
+++ src/main/java/com/clouway/bank/BankBootstrap.java
@@ -0,0 +1,16 @@
+package com.clouway.bank;
+
+/**
+ * @author Miroslav Genov (miroslav.genov@clouway.com)
+ */
+public class BankBootstrap {
+
+  public static void main(String[] args) {
+    final int httpPort = 8080;
+    Jetty jetty = new Jetty(httpPort);
+    jetty.start();
+
+    System.out.println(String.format("Bank is up and running on: %d", httpPort));
+
+  }
+}
