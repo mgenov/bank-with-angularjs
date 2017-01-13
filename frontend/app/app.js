d1aacb0c014a305294ff84fac0219ab3a9fae619
Miroslav Genov <mgenov@gmail.com>
2017-01-13 12:33:48 +0200

    frontend: add tests for TransactionsHistoryCtrl
    Added 2 tests that verify that TransactionsHistoryCtrl is loading
    transactions when controller is created.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,23 +1,26 @@
 var app = angular.module('bankApp', ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider) {
-    $routeProvider
+  $routeProvider
     .when('/', {
-        templateUrl: 'view/index_page.view.html'
+      templateUrl: 'view/index_page.view.html'
     })
     .when('/history', {
-        templateUrl: 'view/transaction_history_page.view.html'
+      templateUrl: 'view/transaction_history_page.view.html'
     })
     .otherwise({
-        redirectTo: '/'
+      redirectTo: '/'
     });
 
-    $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 });
 
 app.controller('TransactionsHistoryCtrl', function($scope, $http) {
-    $http.get("/history")
-        .then(function(response) {
-          $scope.transactions = response.data;
+  // an empty list of transactions should be displayed 
+  // before loading of the history
+  $scope.transactions = [];
+  $http.get("/history")
+    .then(function(response) {
+      $scope.transactions = response.data;
     });
 });
--- frontend/app/app.spec.js
+++ frontend/app/app.spec.js
@@ -1,10 +1,46 @@
 describe('Routing', function() {
-    beforeEach(angular.mock.module('bankApp'));
+  beforeEach(angular.mock.module('bankApp'));
 
-    it('Should map routes to controllers', function() {
-        inject(function($route) {
-            expect($route.routes['/'].templateUrl).toEqual('view/index_page.view.html');
-            expect($route.routes[null].redirectTo).toEqual('/');
-        });
+  it('Should map routes to controllers', function() {
+    inject(function($route) {
+      expect($route.routes['/'].templateUrl).toEqual('view/index_page.view.html');
+      expect($route.routes[null].redirectTo).toEqual('/');
     });
+  });
+});
+
+describe('TransactionsHistoryCtrl', function() {
+  beforeEach(angular.mock.module('bankApp'));
+  var $controller;
+  var $httpBackend;
+
+  beforeEach(inject(function(_$controller_, _$httpBackend_){
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  it ('Should load transaction history on init', inject(function($http) {
+    var $scope = {};
+    $controller('TransactionsHistoryCtrl', { $scope: $scope, $http: $http });
+
+    $httpBackend
+      .expect('GET', '/history')
+      .respond(200, { foo: 'bar' });
+    $httpBackend.flush();
+
+    expect($scope.transactions).toEqual({ foo: 'bar' });
+  }));
+
+  it ('Should load another transaction history', inject(function($http) {
+    var $scope = {};
+    $controller('TransactionsHistoryCtrl', { $scope: $scope, $http: $http });
+
+    $httpBackend
+      .expect('GET', '/history')
+      .respond(200, [1,2,3]);
+    $httpBackend.flush();
+
+    expect($scope.transactions).toEqual([1,2,3]);
+  }));
+
 });
